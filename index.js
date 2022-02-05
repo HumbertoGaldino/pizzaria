fa6f0e19f184a45711a8a8d7a89058994f2dee3f
Humberto Galdino <humberto.galdino@live.com>
2022-02-05 15:16:08 -0300

    Intala EJS, configura views e separa rotas

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 //ARQUIVO DE CONFIGURAÇÃO INICIAL DO PROJETO
+const path = require('path');
 const express = require('express'); //PACOTE
+
+const pizzasRoutes = require('./routes/pizzas.routes');//IMPORTA ROTA PIZZAS
 
 //CONFIGURAÇÕES DO SERVIDOR
 const app = express(); //RETORNO DA FUNÇÃO EXPRESS
 
-app.get('/', (req,res)=>{
-    res.send('Chegou aqui!');
-});
+app.set('view engine','ejs'); //DEFINE O TIPO DE VIEW
+app.set('views', path.join(__dirname, 'views')); //DEFINE A PASTA DAS VIEWS - __dirname(a partir da pasta que esta) + nome da pasta
+
+app.use(pizzasRoutes); //DECLARA A ROTA PARA UTILIZAÇÃO NO ENDPOINT
+
 
 app.listen(5000, () => console.log('Servidor em execução!'));
