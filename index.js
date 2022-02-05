f4d6f9b4032fbfb5540bf9a85c6825795acab63c
Humberto Galdino <humberto.galdino@live.com>
2022-02-05 15:49:44 -0300

    Prepara a rota de formulários do cadastro de usuários

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express'); //PACOTE
 
 const pizzasRoutes = require('./routes/pizzas.routes');//IMPORTA ROTA PIZZAS
+const usuariosRoutes = require('./routes/usuarios.routes');
 
 //CONFIGURAÇÕES DO SERVIDOR
 const app = express(); //RETORNO DA FUNÇÃO EXPRESS
@@ -10,7 +11,10 @@ const app = express(); //RETORNO DA FUNÇÃO EXPRESS
 app.set('view engine','ejs'); //DEFINE O TIPO DE VIEW
 app.set('views', path.join(__dirname, 'views')); //DEFINE A PASTA DAS VIEWS - __dirname(a partir da pasta que esta) + nome da pasta
 
-app.use(pizzasRoutes); //DECLARA A ROTA PARA UTILIZAÇÃO NO ENDPOINT
+app.use(express.json()); //DEFINE QUE AS REQUISIÇÕES SÃO ENVIADAS COMO JSON
+app.use(express.urlencoded({ extended: false }));//INTERPRETA O JSON, ENTENDE O FORMULÁRIO QUE É ENVIADO DO NAVEGADOR
 
+app.use(pizzasRoutes); //DECLARA A ROTA PARA UTILIZAÇÃO NO ENDPOINT
+app.use('/usuarios', usuariosRoutes);//PREFIXO + ROTAS DE USUÁRIOS
 
 app.listen(5000, () => console.log('Servidor em execução!'));
